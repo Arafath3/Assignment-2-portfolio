6f12df586e2429e8bbdf23ab0963edf2c60f49ec
Arafath3 <arafathreja0@gmail.com>
2023-12-16 10:48:14 +1100

    added animation for skill section

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-function sleep(ms){
+// scroll animation
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animation');
+        }
+        else{
+            sec.classList.remove('show-animation');
+        }
+    })
+}
+
+// writing animation
+function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 const phrases = ['Frontend Developer', 'UI/UX designer'];
 const el = document.getElementById('typed');
 let sleepTime = 100;
 let phraseIndex = 0;
-const writeLoop = async() =>{
-  while(true){
-    let curWord = phrases[phraseIndex];
-    for(let i = 0; i < curWord.length; i++){
-        el.innerText = curWord.substring(0,i + 1);
-        await sleep(sleepTime);
+const writeLoop = async () => {
+    while (true) {
+        let curWord = phrases[phraseIndex];
+        for (let i = 0; i < curWord.length; i++) {
+            el.innerText = curWord.substring(0, i + 1);
+            await sleep(sleepTime);
+        }
+        await sleep(sleepTime * 10);
+        for (let i = curWord.length; i > 0; i--) {
+            el.innerText = curWord.substring(0, i - 1);
+            await sleep(sleepTime);
+        }
+        await sleep(sleepTime * 5);
+        if (phraseIndex === phrases.length - 1) {
+            phraseIndex = 0;
+        }
+        else {
+            phraseIndex++;
+        }
     }
-    await sleep(sleepTime * 10);
-    for(let i = curWord.length; i > 0; i--){
-        el.innerText = curWord.substring(0,i - 1);
-        await sleep(sleepTime);
-    }
-    await sleep(sleepTime * 5);
-    if(phraseIndex === phrases.length -1){
-        phraseIndex = 0;
-    }
-    else{
-        phraseIndex++;
-    }
-  }
 };
 writeLoop();
